90c09dce62deb5b0c41563b59fa5a5895c4d452f
Alec Chan <alecchan1@gmail.com>
2016-11-20 12:40:11 -0800

    Update kernel.c

--- kernel_loader/kernel.c
+++ kernel_loader/kernel.c
@@ -1,7 +1,8 @@
 void main()
 {
 
-
+	//pointer points to the first text cell of video memory (0,0)
 	char* video_memory = (char*) 0xb8000;
+	//write the character X to the (0,0) text cell
 	*video_memory = 'X';
 }
