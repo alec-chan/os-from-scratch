bdd9c5dfe3f8542b4cb61aaf06bd6797d44ee08b
Alec Chan <alecchan1@gmail.com>
2016-11-21 18:42:45 -0800

    Update kernel_loader.asm

--- kernel_loader/kernel_loader.asm
+++ kernel_loader/kernel_loader.asm
@@ -1,16 +1,15 @@
 
 ; A boot  sector  that  boots a C kernel  in 32-bit  protected  mode
 [org 0x7c00]
-KERNEL_OFFSET  equ 0xfffe0000   	; This is the  memory  offset  to which  we will  load  our  kernel
-mov [BOOT_DRIVE], dl 		; BIOS  stores  our  boot  drive  in DL, so it's
-				; best to  remember  this  for  later.
-mov bp, 0x9000         		; Set -up the  stack.
+KERNEL_OFFSET  equ 0x1000   	; This is the  memory  offset  where the kernel will be loaded
+mov [BOOT_DRIVE], dl 		; BIOS stores boot drive in DL
+				
+mov bp, 0x9000         		; Set up the  stack.
 mov sp, bp
 mov bx, MSG_REAL_MODE 	; Announce  that we are  starting
 call  print_string      ; booting  from 16-bit  real  mode
-call  load_kernel       ; Load  our  kernel
-call  switch_to_pm      ; Switch  to  protected  mode , from  which
-			; we will  not  return
+call  load_kernel       ; Load  kernel
+call  switch_to_pm      ; Switch  to  protected  mode
 jmp $
 
 
@@ -22,23 +21,23 @@ jmp $
 [bits  16]
 ; load_kernel
 load_kernel:
-	mov bx, MSG_LOAD_KERNEL    ; Print a message  to say we are  loading  the  kernel
+	mov bx, MSG_LOAD_KERNEL    ; Print a message
 	call  print_string
-	mov bx, KERNEL_OFFSET      ; Set -up  parameters  for  our  disk_load  routine , so
-	mov dh, 15                   ; that we load  the  first  15  sectors (excluding
-	mov dl, [BOOT_DRIVE]       ; the  boot  sector) from  the  boot  disk (i.e.  our
-	call  disk_load               ;   kernel  code) to  address  KERNEL_OFFSET
+	mov bx, KERNEL_OFFSET      ; setup  parameters  for disk_load and load first 15 sectors minus the boot sector
+	mov dh, 15                 ; to KERNEL_OFFSET
+	mov dl, [BOOT_DRIVE]       
+	call  disk_load               
 	ret
 
 [bits  32]
-; This is where we  arrive  after  switching  to and  initialising  protected  mode.
+; Arrive here after switching to and initializing protected mode 
 BEGIN_PM:
-	mov ebx , MSG_PROT_MODE ; Use  our 32-bit  print  routine  to
+	mov ebx , MSG_PROT_MODE ; Use 32-bit  print  routine  to
 	call  print_string_pm   ; announce  we are in  protected  mode
-	call  KERNEL_OFFSET     ; Now  jump to the  address  of our  loaded
-				; kernel  code , assume  the  brace  position ,
-				; and  cross  your  fingers.  Here we go!
-jmp $                      	; Hang.
+	call  KERNEL_OFFSET     ; jump to the  address  of the  loaded
+				; kernel  code
+				
+jmp $                      	
 ; Global  variables
 BOOT_DRIVE       db 0
 MSG_REAL_MODE    db "Started  in 16-bit  Real  Mode", 0
